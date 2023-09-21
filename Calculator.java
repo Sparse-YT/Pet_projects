cdca740412668ba99048504e293046f5d2da86c5
Sparse-YT <144548317+Sparse-YT@users.noreply.github.com>
2023-09-21 08:42:56 -0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -8,14 +8,15 @@ public class Calculator {
                System.out.print("Type any # | ");
                double a = keyboard.nextDouble();
                System.out.print("Enter a Symbol 1 = + , 2 = - , 3 = * , 4 = / ,  | ");
-               int b = keyboard.nextDouble();
+               int b = keyboard.nextInt();
                System.out.print("Type another # | ");
-               int c = keyboard.nextDouble();
-               System.out.print("Type 5 for the remainder of #/# | ");
-               int d = keyboard.nextDouble();
+               double c = keyboard.nextDouble();
+               System.out.print("Do you want it to be rounded? 1 = yes , 0 = no , (Rounds up on neg#s , Rounds down on pos#s) | ");
+               int f = keyboard.nextInt();               
+               System.out.print("Type 5 for the remainder of #/# , type 0 to ignore| ");
+               int d = keyboard.nextInt();
 
                double z = 0;
-
 
 
                if (b == 1) {
@@ -37,9 +38,24 @@ public class Calculator {
                if (d == 5) {
                 z = a % c;
                }
+            
+               int x = (int) z;
 
-             System.out.print(z);
+               if (f == 1) {
+                System.out.print(x);
+               }
+            
+               if (f == 0) {
+                System.out.print(z);
+               } 
+             
                 
+
+        }
+
+
+    }
+}   
 
         }
 
