f521974de9f10edb2723e01a2abe587d5b86c883
Sparse-YT <144548317+Sparse-YT@users.noreply.github.com>
2023-09-21 08:43:20 -0400

    Add files via upload

---
+++ FunnyPassword.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FunnyPassword {
+    public static void main(String[] args) {
+           try (Scanner keyboard = new Scanner(System.in)) {
+
+
+            int pw = keyboard.nextInt();
+
+            
+
+
+               if (pw == 1471) {
+                System.out.print("Hello Ash");
+               } else {
+                System.out.print("System image will now eat it self");
+               }
+
+
+
+
+             
+                
+
+        }
+
+
+    }
+}
